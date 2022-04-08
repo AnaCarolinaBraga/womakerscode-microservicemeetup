c8a32eba403d6fbe7e20025d853a86f8bc999c1e
AnaCarolinaBraga <anabraga95@gmail.com>
2022-04-07 23:05:08 -0300

    Adição do model/registrationDTO

--- src/main/java/com/womakerscode/microservicemeetup/model/RegistrationDTO.java
+++ src/main/java/com/womakerscode/microservicemeetup/model/RegistrationDTO.java
@@ -1,4 +1,29 @@
 package com.womakerscode.microservicemeetup.model;
 
-public class RegistrationDTO {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder    //Essas anotações criam os constructos, getters e setters
+public class RegistrationDTO {     //Vamos acrescentar as regras que queremos que os atributos tenham
+
+    //Não precisa dizer que não vai ser vazio porque cria sozinho
+    private Long id;
+
+    @NotEmpty   //Vai dizer que ele não pode ser vazio
+    private String name;
+
+    @NotEmpty   //Vai dizer que ele não pode ser vazio
+    private LocalDate dateOfregistration;
+
+    @NotEmpty   //Vai dizer que ele não pode ser vazio
+    private String registration;
+
 }
